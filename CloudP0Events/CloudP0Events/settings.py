35bc90ac04aa4d3431b36bcc54616122022f7006
Jorge Martín <je.martin@uniandes.edu.co>
2019-01-22 22:44:52 -0500

    Se crea proyecto eventos con una página de inicio de prueba

--- CloudP0Events/CloudP0Events/settings.py
+++ CloudP0Events/CloudP0Events/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=hdnvypsm7d(u+nr!swh#t_=wrpsvp^pm(bag2-z=pn73f-w-v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'eventos',
 ]
 
 MIDDLEWARE = [
